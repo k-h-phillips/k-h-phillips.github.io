84cf57bce785d3a732f107fe07d147a7522304aa
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-10-18 21:24:27 -0400

    moving to a next.js app

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+// The page shows UI unique to the route. 
+// This particular route is for /dashboard because of the folder name in the project structure.
+export default function Dashboard() {
+  return (
+    <h1>Hello, Dashboard Page!</h1>
+  );
+}
