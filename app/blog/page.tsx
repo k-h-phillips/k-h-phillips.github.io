62cd25f9c2bb7a3e5c42ea03a3bdbcff61fee42d
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-11-22 22:34:22 -0500

    Fix some linting problems

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,5 @@
 export default function Blog() {
   return (
-    <div>This is where I'll display blog postings about what I'm currently interested in or reading about.</div>
+    <div>This is where I&apos;ll display blog postings about what I&apos;m currently interested in or reading about.</div>
   );
 }
