7ac0487bd07f92267ccfc2fb2e50b4aceb8f5b6a
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-12-20 17:32:39 -0500

    Undoing the next.config.js

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    env: {
-        NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY,
-        NEXT_PUBLIC_EMAIL_JS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID
-    },
-}
