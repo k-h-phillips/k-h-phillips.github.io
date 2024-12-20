261a3e7c962b3e8d49cf5da6382245deb4d14243
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-12-20 17:15:45 -0500

    Added next.config.js to attempt to fix the environment variables

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY,
+        NEXT_PUBLIC_EMAIL_JS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID
+    },
+}
