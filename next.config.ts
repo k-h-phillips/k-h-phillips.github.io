046af79acc84774ee0cf326d490c6a7c64c943ac
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-12-20 21:44:29 -0500

    Changed basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    basePath: '/k-h-phillips.github.io'
+    basePath: '/'
 };
 
 module.exports = nextConfig;
