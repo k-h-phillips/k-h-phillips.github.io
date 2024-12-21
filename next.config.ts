7b47ee2ce515ad22812e79cc9996c0590497d6b2
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-12-20 21:36:17 -0500

    Fixing the base path?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+    basePath: '/k-h-phillips.github.io'
 };
 
 module.exports = nextConfig;
