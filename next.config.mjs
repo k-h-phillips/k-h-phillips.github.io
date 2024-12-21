a100c1f3c33bb5858fc017819160a3309c670a30
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-12-20 22:23:55 -0500

    Added envs to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     distDir: 'dist',
+    env: {
+        NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY,
+        NEXT_PUBLIC_EMAIL_JS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID,
+    }
 };
 
 export default nextConfig;
