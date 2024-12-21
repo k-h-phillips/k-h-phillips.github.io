f5fc52aa8aa1c4ae4e2166d980e8bd7374ab81bb
Kirsten Phillips <kirstenphillips210@gmail.com>
2024-12-20 21:31:32 -0500

    Trying once again

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    path: "/k-h-phillips.github.io"
 };
 
 module.exports = nextConfig;
